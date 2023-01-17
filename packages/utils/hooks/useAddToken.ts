c0375d7b412232eab9819f29d1b456644cbdcac2
Xiti922 <100534270+Xiti922@users.noreply.github.com>
2023-01-17 20:41:24 +0100

    Update useAddToken.ts

--- packages/utils/hooks/useAddToken.ts
+++ packages/utils/hooks/useAddToken.ts
@@ -1,5 +1,5 @@
 import { getKeplrFromWindow } from '@keplr-wallet/stores'
-import { ChainInfoID } from '@noahsaso/cosmodal'
+import { ChainInfoID } from '@xiti/cosmodal'
 import { useMemo } from 'react'
 import toast from 'react-hot-toast'
 import { useTranslation } from 'react-i18next'
